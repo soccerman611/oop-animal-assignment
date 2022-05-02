298729af9a6a1d3aa0d227fe8b9d155152069d74
soccerman611 <81501902+soccerman611@users.noreply.github.com>
2022-05-02 08:54:30 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+i am doing a little bit of working and experimenting
+
+
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
